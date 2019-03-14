3c710a31b88fec3675485998b59759ff67b8ccf8
pszczolkowski <pszczolkowski.k@gmail.com>
2019-03-14 19:06:12 +0000

    posting photo

---
+++ camera.js
@@ -0,0 +1,62 @@
+var NodeWebcam = require( "node-webcam" );
+ 
+//Default options
+var opts = {
+ 
+    //Picture related
+    width: 1280,
+    height: 720,
+    quality: 100,
+ 
+    //Delay to take shot
+    delay: 0,
+ 
+    //Save shots in memory
+    saveShots: true,
+ 
+    // [jpeg, png] support varies
+    // Webcam.OutputTypes
+    output: "jpeg",
+ 
+    //Which camera to use
+    //Use Webcam.list() for results
+    //false for default device
+    device: false,
+ 
+ 
+    // [location, buffer, base64]
+    // Webcam.CallbackReturnTypes
+    callbackReturn: "location",
+ 
+    //Logging
+    verbose: false
+ 
+};
+ 
+ 
+//Creates webcam instance
+ 
+var Webcam = NodeWebcam.create(opts);
+ 
+ 
+//Will automatically append location output type
+
+function takePhoto() {
+    return new Promise((resolve, reject) => {
+        Webcam.capture( "test_picture", function( err, data ) {
+            if (err) {
+                    reject(err);
+            } else {
+                resolve(data);
+            }
+        } );
+    }); 
+}
+
+module.exports = {
+    takePhoto
+};
+
+ 
+
+
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { addEnterOfficeListener, addExitOfficeListener } = require('./enter-exit.js');
+/*const { addEnterOfficeListener, addExitOfficeListener } = require('./enter-exit.js');
 
 addEnterOfficeListener(() => {
 	console.log('WE HAVE AN ENTER MOTHERFUCKER');
@@ -6,4 +6,31 @@ addEnterOfficeListener(() => {
 
 addExitOfficeListener(() => {
 	console.log('OMG SOMEONE IS LEAVING US');
-});
+});*/
+
+/*const { takePhoto } = require('./camera.js');
+
+takePhoto().then(photoPath => {
+   sendPhoto(photo); 
+});*/
+const fs = require('fs');
+const request = require('request');
+var FormData = require('form-data');
+sendPhoto(__dirname + '/test_picture.jpg');
+
+function sendPhoto(photoPath) {
+    var form = new FormData();
+    form.append('file', fs.createReadStream(photoPath));
+    form.submit("http://3e96b120.ngrok.io/confluence/rest/usersresource/1.0/users/photo", (err, resp) => {
+        console.log(err, resp);
+    });
+    
+    //var req = request.post("http://3e96b120.ngrok.io/confluence/rest/usersresource/1.0/users/photo", function (err, resp, body) {
+      
+/*       request.post({url:"http://3e96b120.ngrok.io/confluence/rest/usersresource/1.0/users/photo", formData: form}, function optionalCallback(err, httpResponse, body) {
+          if (err) {
+            return console.error('upload failed:', err);
+          }
+          console.log('Upload successful!  Server responded with:', body);
+        }); */
+}
